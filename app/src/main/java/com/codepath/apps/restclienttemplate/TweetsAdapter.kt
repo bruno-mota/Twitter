210c41c3ce7ce67414a4f8a2de5862b36a7e0563
Bruno Mota <bruno.ed.mota@gmail.com>
2022-10-09 14:09:16 -0400

    Completed User Stories

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -0,0 +1,51 @@
+package com.codepath.apps.restclienttemplate
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+import com.codepath.apps.restclienttemplate.models.Tweet
+
+class TweetsAdapter(val tweets: ArrayList<Tweet>):RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.item_tweet,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TweetsAdapter.ViewHolder, position: Int) {
+        val tweet: Tweet = tweets.get(position)
+        holder.tvUser.text = tweet.user?.name
+        holder.tvTweetBOdy.text = tweet.body
+
+        Glide.with(holder.itemView.context).load(tweet.user?.publicImageUrl)
+            .into(holder.ivProfileImage)
+        
+    }
+
+    override fun getItemCount(): Int {
+        return tweets.size
+    }
+    fun clear(){
+        tweets.clear()
+        notifyDataSetChanged()
+    }
+    fun addAll(tweetList:List<Tweet>){
+        tweets.addAll(tweetList)
+        notifyDataSetChanged()
+    }
+    class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+        val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
+        val tvTweetBOdy = itemView.findViewById<TextView>(R.id.tvTweetBody)
+        val tvUser = itemView.findViewById<TextView>(R.id.tvUsername)
+    }
+}
